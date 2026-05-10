a8f5880f4edf042dd96830fdf69c62f0607ce016
Pismeno <krystof.kloz@gmail.com>
2026-05-10 17:10:48 +0200

    feat(editor): Glfw Window implementation

--- src/bird_editor/glfw_window.cpp
+++ src/bird_editor/glfw_window.cpp
@@ -2,5 +2,33 @@
 
 namespace bird::editor {
 
+GlfwWindow::GlfwWindow(int width, int height, const std::string& title) {
+  this->data.width = width;
+  this->data.height = height;
+  this->data.title = title;
+
+  this->window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+
+  glfwMakeContextCurrent(window);
+}
+
+GlfwWindow::~GlfwWindow() {}
+
+void GlfwWindow::onUpdate() {
+  glfwPollEvents();
+  glfwSwapBuffers(window);
+}
+
+bool GlfwWindow::shouldClose() const {
+  return glfwWindowShouldClose(window);
+}
+
+uint32_t GlfwWindow::getWidth() const {
+  return this->data.width;
+}
+
+uint32_t GlfwWindow::getHeight() const {
+  return this->data.height;
+}
 
 } // bird::editor
--- src/bird_editor/main.cpp
+++ src/bird_editor/main.cpp
@@ -17,7 +17,7 @@ int main() {
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
+
 
     std::cout << "Hello, Bird Editor!" << std::endl;
 
